ef0ef2485be1293b3cac2e6921cb0f379fad9c31
Honey <72255816+Honeyrn@users.noreply.github.com>
2023-01-09 03:15:38 +0530

    Add files via upload

---
+++ Invoices/index.js
@@ -0,0 +1,187 @@
+
+let myArray = [
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+    {
+        "invoice ": 2,
+        "clientName": "Edwin",
+        "clientType": "In-State",
+        "Date": "9/11/2019",
+        "DueDate": "13/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status": Draft
+    },
+    {
+        "invoice ": 2,
+        "clientName": "Edwin",
+        "clientType": "In-State",
+        "Date": "3/11/2019",
+        "DueDate": "14/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status": Draft
+    },
+    {
+        "invoice ": 2,
+        "clientName": "Edwin",
+        "clientType": "In-State",
+        "Date": "1/11/2019",
+        "DueDate": "15/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status": Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "Out-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "Out-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "Out-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+
+    {
+        "invoice" : 1,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : Draft
+    },
+]
+
+
+function changeColor(id){
+
+    if(document.getElementById("B")){
+        let a = document.getElementById("B");
+        a.style.backgroundColor="#4351d3";
+    }
+    else if(document.getElementById("E")){
+        let b = document.getElementById("E");
+        b.style.backgroundColor="#4351d3";
+    }
+    else if(document.getElementById("S")){
+        let b = document.getElementById("S");
+        b.style.backgroundColor="#4351d3";
+    }
+    else if(document.getElementById("C")){
+        let b = document.getElementById("C");
+        b.style.backgroundColor="#4351d3";
+    }
+
+}
