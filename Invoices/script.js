8ebcf57cbca9dfbf12b78ddd4a8ace6d9149f6ad
Honey <72255816+Honeyrn@users.noreply.github.com>
2023-01-15 12:36:22 +0530

    Update and rename index.js to script.js

--- Invoices/script.js
+++ Invoices/script.js
@@ -1,5 +1,5 @@
 
-let myArray = [
+let data = [
 
     {
         "invoice" : 1,
@@ -9,179 +9,195 @@ let myArray = [
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
     {
-        "invoice ": 2,
-        "clientName": "Edwin",
-        "clientType": "In-State",
-        "Date": "9/11/2019",
-        "DueDate": "13/11/2019",
-        "Total": 678,
-        "Balance": 678,
-        "Status": Draft
-    },
-    {
-        "invoice ": 2,
-        "clientName": "Edwin",
-        "clientType": "In-State",
-        "Date": "3/11/2019",
-        "DueDate": "14/11/2019",
-        "Total": 678,
-        "Balance": 678,
-        "Status": Draft
-    },
-    {
-        "invoice ": 2,
-        "clientName": "Edwin",
-        "clientType": "In-State",
-        "Date": "1/11/2019",
-        "DueDate": "15/11/2019",
-        "Total": 678,
-        "Balance": 678,
-        "Status": Draft
-    },
-
-    {
-        "invoice" : 1,
+        "invoice" : 2,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
-
     {
-        "invoice" : 1,
+        "invoice" : 3,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
-
     {
-        "invoice" : 1,
+        "invoice" : 10,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
-
     {
-        "invoice" : 1,
+        "invoice" : 5,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
 
     {
-        "invoice" : 1,
+        "invoice" : 6,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Partial-Payment"
     },
 
     {
-        "invoice" : 1,
-        "clientName": "Thomas",
-        "clientType": "Out-State",
-        "Date": "5/11/2019",
-        "DueDate": "12/11/2019",
-        "Total": 678,
-        "Balance": 678,
-        "Status" : Draft
-    },
-
-    {
-        "invoice" : 1,
+        "invoice" : 7,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Paid"
     },
 
     {
-        "invoice" : 1,
+        "invoice" : 8,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Paid"
     },
 
     {
-        "invoice" : 1,
+        "invoice" : 9,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : "Partial-Payment"
+    },
+
+    {
+        "invoice" : 4,
         "clientName": "Thomas",
         "clientType": "Out-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Draft"
     },
 
     {
-        "invoice" : 1,
-        "clientName": "Thomas",
-        "clientType": "Out-State",
-        "Date": "5/11/2019",
-        "DueDate": "12/11/2019",
-        "Total": 678,
-        "Balance": 678,
-        "Status" : Draft
-    },
-
-    {
-        "invoice" : 1,
+        "invoice" : 11,
         "clientName": "Thomas",
         "clientType": "In-State",
         "Date": "5/11/2019",
         "DueDate": "12/11/2019",
         "Total": 678,
         "Balance": 678,
-        "Status" : Draft
+        "Status" : "Paid"
+    },
+
+    {
+        "invoice" : 12,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : "Draft"
+    },
+
+    {
+        "invoice" : 13,
+        "clientName": "Thomas",
+        "clientType": "Out-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : "Draft"
+    },
+
+    {
+        "invoice" : 14,
+        "clientName": "Thomas",
+        "clientType": "Out-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : "Partial-Payment"
+    },
+
+    {
+        "invoice" : 15,
+        "clientName": "Thomas",
+        "clientType": "In-State",
+        "Date": "5/11/2019",
+        "DueDate": "12/11/2019",
+        "Total": 678,
+        "Balance": 678,
+        "Status" : "Paid"
     },
 ]
 
 
-function changeColor(id){
+// var element = document.querySelector("#table-container")
+// element.classList.add("Paid")
 
-    if(document.getElementById("B")){
-        let a = document.getElementById("B");
-        a.style.backgroundColor="#4351d3";
-    }
-    else if(document.getElementById("E")){
-        let b = document.getElementById("E");
-        b.style.backgroundColor="#4351d3";
-    }
-    else if(document.getElementById("S")){
-        let b = document.getElementById("S");
-        b.style.backgroundColor="#4351d3";
-    }
-    else if(document.getElementById("C")){
-        let b = document.getElementById("C");
-        b.style.backgroundColor="#4351d3";
-    }
 
+var table = document.createElement("table");
+var body = table.createTBody();
+for (var i = 0; i < data.length; i++) {
+    var row = body.insertRow();
+    var obj = data[i];
+    for (var key in obj) {
+        var cell = row.insertCell();
+        // console.log(key);
+        
+        cell.innerHTML = obj[key];
+
+
+    }
 }
+
+document.getElementById("table-container").appendChild(table);
+
+
+// for(var i=0; i<data.length; i++){
+//     var invoices = data[i]['invoice'];
+//     var clientName = data[i]['clientName'];
+//     console.log(invoices);
+//     console.log(clientName);
+// }
+
+function changeColor(){
+    var b = document.getElementById("B");
+    b.addEventListener("click", function() {
+    b.style.backgroundColor = "#4351d3";
+    b.style.color = "white";
+  });
+}
+
